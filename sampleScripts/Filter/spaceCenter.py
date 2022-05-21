1235976ec6a2a185847cac46dc31f75c0091b512
typoman <eslami.bahman@gmail.com>
2022-05-21 19:29:13 +0200

    Add filter to space center glyphs.

---
+++ sampleScripts/Filter/spaceCenter.py
@@ -0,0 +1,27 @@
+from mojo.UI import CurrentSpaceCenter
+from selection import *
+"""
+Filter the glyph set in the view to the glyphs inside the space center.
+"""
+
+def geComps(g):
+    return set([c.baseGlyph for c in g.components])
+
+def limitVeiwToSpaceCenter(includingComposites=True):
+    f = CurrentFont()
+    glyphSet = set(f.keys())
+    sp = CurrentSpaceCenter()
+    gl = set(sp.get()) & glyphSet
+    finalList = set(gl)
+    if includingComposites:
+        for g in gl:
+            g = f[g]
+            for c in g.components:
+                finalList.add(c.baseGlyph)
+        for g in f:
+            if not g.contours and geComps(g) - finalList == set():
+                finalList.add(g.name)
+
+    limitFontViewToGlyphList(finalList)
+
+limitVeiwToSpaceCenter(1)
